a8c8387ff1fd3b73c15ac34ae87d7773e08f2fa3
Ustym Buchko <92269527+Ustymko@users.noreply.github.com>
2023-06-19 22:45:27 +0300

    home screen server adaptation

--- screens/HomeScreen/index.js
+++ screens/HomeScreen/index.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, isSelectionModeEnabled, disableSelectionMode} from 'react';
+import React, {useEffect, useState} from 'react';
 import { View, Text } from 'react-native';
 import HomeScreenButtons from '../../components/HomeScreenButtons';
 import HomeScreenTopPanel from '../../components/HomeScreenTopPanel';
 import ParkingPlaceCloseToUser from '../../components/ParkingPlaceCloseToUser';
 import styles from './style';
-import { useNavigation } from '@react-navigation/native';
+
 const HomeScreen = () => {
+
+  const [parkingPlacesRendered, setParkingPlacesRendered] = useState()
+
+  const parkingPlaces = [
+    {owner: "Ta*** ***ba", address: "Kharkivska 4", price: "20 hrn/hour"},
+    {owner: "Yu*** **********iy", address: "Kharkivska 5", price: "15 hrn/hour"},
+    {owner: "Yu*** *******yk", address: "Kharkivska 6", price: "17 hrn/hour"}
+  ]
+
+  useEffect(() => {
+    // here parking places should be retrieved from the server using fetch api
+    setParkingPlacesRendered(
+      parkingPlaces.map((item, index) => {
+        return(
+          <ParkingPlaceCloseToUser key={index} owner={item.owner} address={item.address} price={item.price}/>
+        )
+      })
+    )  
+  }, [])
 
     return (
       <View style={styles.container}>
         <HomeScreenTopPanel/>
         <Text style={styles.placesCloseToYou}>Places close to you</Text>
-        <ParkingPlaceCloseToUser owner={"Ta*** ***ba"} address={"Kharkivska 4"} price={"20 hrn/hour"}/>
-        <ParkingPlaceCloseToUser owner={"Yu*** **********iy"} address={"Kharkivska 5"} price={"15 hrn/hour"}/>
-        <ParkingPlaceCloseToUser owner={"Yu*** *******yk"} address={"Kharkivska 6"} price={"17 hrn/hour"}/>
+        {parkingPlacesRendered}
           <View style={styles.homeScreenButtons}>
             <HomeScreenButtons/>
           </View>
